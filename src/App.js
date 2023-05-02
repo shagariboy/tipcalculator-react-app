daadb74d32696a7cefb91e4d0f0dc0123c572996
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-02 08:51:53 -0400

    Create tip calculator class
    Export component
    Create corresponding CSS and link
    Update classNames to conform with JSX
    Render in APP.JS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './Components/Header';
+import TipCalculator from './Components/TipCalculator/TipCalculator';
 
 function App() {
   return (
     <div className="main-container">
       <Header />
+      <TipCalculator/>
     </div>
   );
 }
---
+++ src/Components/TipCalculator/TipCalculator.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+import "./TipCalculator.css"
+
+class TipCalculator extends Component {
+    render() {
+        return (
+            <div className="tip-calculator">
+                <div className="label-wrapper">
+                    <label htmlFor="tip">Select Tip %</label>
+                </div>
+                <div className="btn-wrapper">
+                    <div className="btn tip">5%</div>
+                    <div className="btn tip">10%</div>
+                    <div className="btn tip btn-active">15%</div>
+                    <div className="btn tip">25%</div>
+                    <div className="btn tip">50%</div>
+                    <input className="input-text" type="text" name="tip" id="tip" placeholder="Custom" />
+                </div>
+                <div className="people-container">
+                    <div className="label-wrapper">
+                        <label htmlFor="people">Number of People</label>
+                        <div className="error-note">Can't be zero</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TipCalculator;
