79eefabb6d33635669593c2de917079a31381623
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-05-17 20:24:19 -0400

    Change Class to className

--- src/Components/LineWrapper/LineWrapper.js
+++ src/Components/LineWrapper/LineWrapper.js
@@ -7,13 +7,13 @@ import './LineWrapper.css';
 class LineWrapper extends Component {
     render() {
         return (
-            <div className="line-wrapper">
-                <div class="line-output">
-                    <div class="title-wrapper">
-                        <div class="title">Tip Amount</div>
-                        <div class="desc">/ person</div>
+            <div classNameName="line-wrapper">
+                <div className="line-output">
+                    <div className="title-wrapper">
+                        <div className="title">Tip Amount</div>
+                        <div className="desc">/ person</div>
                     </div>
-                    <div class="value">$0.00</div>
+                    <div className="value">$0.00</div>
                 </div>
             </div>
         );
